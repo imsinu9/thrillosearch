21c0bbb9fc5adebee2e52e9ea9ca53ecf3188855
Prateek Mittal <prateek.mittal@bookmyshow.com>
2015-08-17 23:24:58 +0530

    Improving Indentation

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -10,26 +10,26 @@ class Tour < ActiveRecord::Base
 		indexes :tags, type: 'string'
 	end
 
-  	belongs_to :city
-  	belongs_to :tag
+	belongs_to :city
+	belongs_to :tag
 
-  	validates_presence_of :city, :name
+	validates_presence_of :city, :name
 
-  	after_commit :index_document, on: :create
-  	after_commit :update_document, on: :update 
-  	after_commit :delete_document, on: :destroy 
+	after_commit :index_document, on: :create
+	after_commit :update_document, on: :update 
+	after_commit :delete_document, on: :destroy 
 
-  	class << self
-  		def search(query)
-  			Searchable.search(query).results
-  		end		
-  	end
+	class << self
+		def search(query)
+			Searchable.search(query).results
+		end		
+	end
 
-  	def tags
-  		tags = [self.city.name]
+	def tags
+		tags = [self.city.name]
 
-  		if self.tag.present?
-  			tags << self.tag.name 
-  		end
-  	end
+		if self.tag.present?
+			tags << self.tag.name 
+		end
+	end
 end
