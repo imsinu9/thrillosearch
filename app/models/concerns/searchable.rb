7a20cf88bba527b393ff08356a7911d45bf2a55c
Prateek Mittal <prateek.mittal@bookmyshow.com>
2015-08-18 20:15:46 +0530

    Improving search query

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -5,17 +5,25 @@ module Searchable
         Elasticsearch::Model.search( search_query(query), types, options={}).results
     end
 
-    def self.search_query(query)
+    def self.search_query(q)
        query = {
             query: {
                 bool: {
-                    should: {
-                        query_string: {
-                            fields: fields,
-                            defaultOperator: 'or',
-                            query: query
+                    should: [
+                        {
+                            query_string: {
+                                fields: fields,
+                                allow_leading_wildcard: false,
+                                query: q
+                            }
+                        },
+                        {
+                            terms: {
+                                tags: tag_builder(q),
+                                minimum_should_match: tag_length(q)
+                            }
                         }
-                    }
+                    ]
                 }
             },
             size: 6
@@ -28,5 +36,13 @@ module Searchable
 
     def self.fields
         ['name.name_default^3', 'name.ngrams_front']
+    end
+
+    def self.tag_builder(query)
+        query.split(' ')
+    end
+
+    def self.tag_length(query)
+        query.split(' ').length
     end
 end
