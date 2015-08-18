fd88dbc4228786baa48618308a65dd9f9b53e7e0
Prateek Mittal <prateek.mittal@bookmyshow.com>
2015-08-18 21:31:45 +0530

    Preetifying result

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,21 @@ class SearchController < ApplicationController
 	def search
 		q = params[:q]
 
-		@results = Tour.search(q)
-		render json: @results
+		results = Tour.search(q)
+		format_json(results)
+		
+		render json: @out
+	end
+
+	def format_json(results)
+		@out = []
+
+		results.each do |result|
+			@out <<	{
+				name: result._source.name,
+				code: result._id,
+				type: result._type
+			}
+		end
 	end
 end
