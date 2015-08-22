9e8f66c76b5601cc3cbb92732265df5a78156d8b
Prateek Mittal <prateek.mittal@bookmyshow.com>
2015-08-22 16:48:29 +0530

    Adding some more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,25 @@ json = ActiveSupport::JSON.decode(File.read('db/seeds.json'))
 
 puts "Seeding Cities"
 json["cities"].each do |city|
-  City.create!(name: city['name'])
+    City.create!(name: city['name'])
 end
 puts "#{json['cities'].length} Cities Added"
 
 puts "Seeding Tags"
 json["tags"].each do |tag|
-  Tag.create!(name: tag['name'])
+    Tag.create!(name: tag['name'])
 end
 puts "#{json['tags'].length} Tags Added"
 
 puts "Seeding Tours"
 json["tours"].each do |tour|
-  city = City.find_by_name(tour['city_name'].titleize)
-  Tour.create!(name: tour['name'], city: city)
+    city = City.find_by_name(tour['city_name'].titleize)
+
+    if tour['tag_name'].present?
+        tag = Tag.find_by_name(tour['tag_name'].titleize)
+        Tour.create!(name: tour['name'], city: city, tag: tag)
+    else
+        Tour.create!(name: tour['name'], city: city)
+    end
 end
 puts "#{json['tours'].length} Tours Added"
